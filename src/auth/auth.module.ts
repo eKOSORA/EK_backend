2b6d5c618ec9b28878eba83009c30f55b26eede5
IVainqueur <ishimvainqueur@gmail.com>
2022-09-01 19:00:16 +0200

    Auth config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SignupController } from './controllers/signup/signup.controller';
+import { ControllerController } from './controllers/login/controller/controller.controller';
+import { SignupService } from './services/signup/signup.service';
+import { LoginService } from './controllers/login/login.service';
 
-@Module({})
+@Module({
+  controllers: [SignupController, ControllerController],
+  providers: [SignupService, LoginService]
+})
 export class AuthModule {}
