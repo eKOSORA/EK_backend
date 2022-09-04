9507bc0c76f44826cc15d95ef852d3c3eb4d71c6
IVainqueur <ishimvainqueur@gmail.com>
2022-09-04 21:09:33 +0200

    Adding educator and subject schemas

---
+++ src/schemas/educator.schema.ts
@@ -0,0 +1,34 @@
+import { Subject } from './subject.schema';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export type EducatorDocument = Educator & mongoose.Document;
+
+@Schema()
+export class Educator {
+  @Prop({ type: String })
+  names: string;
+
+  @Prop({ type: String })
+  code: string;
+
+  @Prop({ type: String })
+  title: string;
+
+  @Prop({ type: String })
+  tel: string;
+
+  @Prop({ type: String })
+  email: string;
+
+  @Prop({ type: String })
+  password: string;
+
+  @Prop({ type: [Subject] })
+  subjects: Subject[];
+
+  @Prop({ type: String })
+  profileLink: string;
+}
+
+export const EducatorSchema = SchemaFactory.createForClass(Educator);
---
+++ src/schemas/subject.schema.ts
@@ -0,0 +1,19 @@
+import { School } from './school.schema';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export type SubjectDocument = Subject & mongoose.Document;
+
+@Schema()
+export class Subject {
+  @Prop({ type: String, unique: true, lowercase: true, trim: true })
+  title: string;
+
+  @Prop({ type: String })
+  code: string;
+
+  @Prop({ type: [School] })
+  schools: School[];
+}
+
+export const SubjectSchema = SchemaFactory.createForClass(Subject);
