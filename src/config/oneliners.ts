457155e907aa840be7a9fee8d13b7ee0601e323f
IVainqueur <ishimvainqueur@gmail.com>
2022-09-05 18:53:10 +0200

    Adding a oneliners file

---
+++ src/config/oneliners.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+export const chalk = require('chalk');
+
+export const red = chalk.red;
+export const green = chalk.green;
+export const yellow = chalk.yellow;
+export const checkEmoji = '🔴';
